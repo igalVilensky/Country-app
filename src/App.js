29658674d705acb27f307634946fb23b3ffa73f2
igalv <vilenskyigal@gmail.com>
2021-06-08 13:11:09 +0200

    structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "./scss/main.scss";
+import axios from "axios";
 
 function App() {
+  const [userInput, setUserInput] = useState("");
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
+
+  function changeHandle(e) {
+    setUserInput(e.target.value);
+  }
+
+  function submitHandle(e) {
+    e.preventDefault();
+    let textToUrl = encodeURIComponent(userInput);
+    let endPoint = `https://restcountries.eu/rest/v2/name/${userInput}`;
+    // console.log(textToUrl);
+
+    axios(endPoint)
+      .then(({ data }) => setResults(data))
+      .catch((err) => console.log(`You have an ${err}`));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <form onSubmit={submitHandle}>
+        <input
+          type="text"
+          value={userInput}
+          onChange={changeHandle}
+          placeholder="Please write a country name"
+        />
+        <button type="submit">Search</button>
+      </form>
+    </React.Fragment>
   );
 }
 
